a151f93c11cc4c28642c3195f5c32423f4d5d03d
Sebastián Astiazarán <65567568+Sastiazaran@users.noreply.github.com>
2024-01-29 09:57:08 -0600

    First activity

---
+++ Index/Index/kernel.cu
@@ -0,0 +1,21 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+__global__ void print()
+{
+    int i = threadIdx.x;
+    printf("[DEVICE] ThreadIdx.x: %d\n", i);
+}
+
+int main()
+{
+    print << <2, 8 >> > ();
+
+    return 0;
+}
+
+
