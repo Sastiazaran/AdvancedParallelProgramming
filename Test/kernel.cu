2d823cf65d088ea9b0b2ac20d697f17386b00d2a
unknown <CIAC@CIAC2-004.academic.mixcoac.upmx.mx>
2024-02-19 09:53:36 -0600

    test prob

---
+++ Test/kernel.cu
@@ -0,0 +1,39 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void vectorBitwiseOR(const unsigned char *A, const unsigned char *B, unsigned char *C, int size )
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid < size) {
+        C[tid] = A[tid] | B[tid];
+    }
+}
+
+int main()
+{
+
+    const int size_in_bytes = 1024;
+    unsigned char A[size_in_bytes];
+    unsigned char B[size_in_bytes];
+    unsigned char C[size_in_bytes];
+    int N_size;
+    int total_threads;
+    int threads_per_block;
+
+    int blocks;
+
+    N_size = size_in_bytes / sizeof(unsigned char);
+    total_threads = 1024 * 46;
+    threads_per_block = 1024 / 16;
+    blocks = (N_size + threads_per_block - 1) / threads_per_block;
+
+    vectorBitwiseOR << <blocks, threads_per_block >> > (A, B, C, N_size);
+
+    printf("Nsize: %d", N_size);
+
+    return 0;
+
+}
